fc2a88bab7e6fb48436f5699db347a2e08079440
Rifki Fauzi <kubido@gmail.com>
2018-05-17 14:48:57 +0700

    working watcher

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const argv = process.argv.slice(2)
 const sessionName = argv[0]
 
 const Watcher = require('./lib/watcher')
-const watcher= new Watcher(__dirname, sessionName)
+const watcher = new Watcher(__dirname, sessionName)
+
 watcher.initialize()
 
