032f35581589fe11a903964f95960836eab317e5
Rifki Fauzi <kubido@gmail.com>
2018-05-22 11:40:49 +0700

    handle multiple sessions

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 const argv = process.argv.slice(2)
 const sessionName = argv[0]
+const host = argv[1]
 
 const Watcher = require('./lib/watcher')
-const watcher = new Watcher(__dirname, sessionName)
+const watcher = new Watcher(__dirname, sessionName, host)
 
 watcher.initialize()
